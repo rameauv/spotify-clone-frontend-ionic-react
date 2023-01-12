ccfee043932213cec2c51de1e15375974468097f
valentin rameau <valentin.rameau@outlook.fr>
2023-01-12 13:10:29 +0900

    feat(es-lint): typescript config

--- src/components/discard-modal/discard-modal-controller.tsx
+++ src/components/discard-modal/discard-modal-controller.tsx
@@ -1,48 +1,10 @@
 import React from 'react';
-import {Animation, createAnimation, IonModal} from '@ionic/react';
+import {IonModal} from '@ionic/react';
 import styles from './discard-modal.module.scss';
 import TextButton from '../buttons/text-button/text-button';
 import RegularButton from '../buttons/regular-button/regular-button';
-
-const leaveAnimation = (baseEl: HTMLElement): Animation => {
-    const baseAnimation = createAnimation();
-    const backdropAnimation = createAnimation();
-    const wrapperAnimation = createAnimation();
-
-    backdropAnimation.addElement(baseEl.shadowRoot?.querySelector('ion-backdrop')!).fromTo('opacity', 'var(--backdrop-opacity)', 0);
-
-    wrapperAnimation.addElement(baseEl.shadowRoot?.querySelector('.modal-wrapper')!).fromTo('opacity', 0.99, 0);
-
-    return baseAnimation
-        .addElement(baseEl)
-        .easing('ease-in-out')
-        .duration(150)
-        .addAnimation([backdropAnimation, wrapperAnimation]);
-};
-
-const enterAnimation = (baseEl: HTMLElement): Animation => {
-    const baseAnimation = createAnimation();
-    const backdropAnimation = createAnimation();
-    const wrapperAnimation = createAnimation();
-
-    backdropAnimation
-        .addElement(baseEl.shadowRoot?.querySelector('ion-backdrop')!)
-        .fromTo('opacity', 0.01, 'var(--backdrop-opacity)')
-        .beforeStyles({
-            'pointer-events': 'none',
-        })
-        .afterClearStyles(['pointer-events']);
-    wrapperAnimation.addElement(baseEl.shadowRoot?.querySelector('.modal-wrapper')!).keyframes([
-        {offset: 0, opacity: '0.01', transform: 'scale(0.9)'},
-        {offset: 1, opacity: '1', transform: 'scale(1)'},
-    ]);
-
-    return baseAnimation
-        .addElement(baseEl)
-        .easing('ease-in-out')
-        .duration(150)
-        .addAnimation([backdropAnimation, wrapperAnimation]);
-};
+import enterAnimation from '../../animations/enter-animation';
+import leaveAnimation from '../../animations/leave-animation';
 
 interface EditProfileModalControllerProps {
     isOpen?: boolean;
