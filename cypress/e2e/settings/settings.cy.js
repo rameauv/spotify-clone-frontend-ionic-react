f65317cded10b481e56f7d8069b65f1822de9d44
rameauv <valentin.rameau@outlook.fr>
2023-01-19 16:05:11 +0900

    feat(api-change) (#9)
    - change the e2e tests to be compatible with the new api

--- cypress/e2e/settings/settings.cy.js
+++ cypress/e2e/settings/settings.cy.js
@@ -10,16 +10,17 @@ describe('settings', {
   })
 
   it('should navigate to the settings page from the library tab and chagen the profile name to nameChanged', () => {
-    const newName = 'nameChanged';
+    const change = 'Changed';
+    const changedName = credentials.userName + change;
+
     cy.login({userName: credentials.userName, password: credentials.password});
     cy.get('#tab-button-library').click();
     cy.get('.library_profileIcon__sqKrD').click();
     cy.get('.settings_profileSettingsButton__P3y6Z').click();
-    //cy.get('.profile-settings_ionContent__5wkpg', {includeShadowDom: true}).shadow().find('.inner-scroll').scrollTo(0, {duration: 5000});
     cy.get('.round-outline-button_editButton__z6fol').click({scrollBehavior: false});
-    cy.get('.edit-profile-modal_profileTitleInput__vJVXc').type(newName);
+    cy.get('.edit-profile-modal_profileTitleInput__vJVXc').type(change);
     cy.get('.text-button_button__MBibQ').click();
     cy.get('.profile-settings_profileSettingsButton__profileTitle__ax36u')
-        .should('have.text', newName)
+        .should('have.text', changedName)
   });
 });
